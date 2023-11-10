73dcb15f5fb98a6ba05d48c97a4c8079e9420f17
Rapanda0 <ryel.rapada07@gmail.com>
2023-11-10 00:23:58 -0700

    "lab 2 stuff"

---
+++ App.jsx
@@ -0,0 +1,52 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Button
+} from 'react-native';
+import ToDoList from './ToDoList';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList />  
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a new task..."
+        />
+        <Button title="Add" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
+
+export default App;
