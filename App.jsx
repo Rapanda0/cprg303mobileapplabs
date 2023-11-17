d972cdee54c9b29c9df53b46ed6d242d67b2a931
Rapanda0 <ryel.rapada07@gmail.com>
2023-11-16 20:31:20 -0700

    lab 4 fixes

--- App.jsx
+++ App.jsx
@@ -1,66 +1,27 @@
-/**
- * My To Do List App
- *
- * @format
- */
-import React from 'react';
-import { useState } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Pressable,
-  View,
-  Text,
-  ScrollView,
-  TextInput,
-  Button
-} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native';
 import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
 
-const defaultTasks = [
-  'Do laundry',
-  'Go to gym',
-  'Walk dog'
-]
 function App() {
-  
-const [tasks, setTasks] = useState(defaultTasks);
+  const defaultTasks = [
+    'Do laundry', 
+    'Go to gym', 
+    'Walk dog'
+];
 
-//useless for now 
-const addTask = (task) => {
-  setTasks([...tasks, task]);
-}
+  const [tasks, setTasks] = useState(defaultTasks);
+
+  const addTask = (task) => {
+      setTasks([...tasks, task]);
+  };
 
   return (
-    <SafeAreaView>
-      <ToDoList tasks={tasks}/>  
-      <View style={styles.form}>
-        <TextInput
-          style={styles.input}
-          placeholder="Add a new task..."
-        />
-        <Button title="Add" />
-      </View>
-    </SafeAreaView>
+      <SafeAreaView>
+        <ToDoList tasks={tasks} />
+        <ToDoForm addTask={addTask}/>
+      </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  form: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginHorizontal: 20,
-    marginTop: 20,
-  },
-  input: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    marginRight: 10,
-  },
-});
 
 export default App;
--- ToDoList.jsx
+++ ToDoList.jsx
@@ -1,43 +1,38 @@
-
 import React from 'react';
-
-import {
-  SafeAreaView,
-  StyleSheet,
-  Pressable,
-  View,
-  Text,
-  ScrollView,
-  TextInput,
-  Button,
-
+import { 
+    ScrollView, 
+    StyleSheet, 
+    Pressable, 
+    View, 
+    Text 
 } from 'react-native';
 
 function ToDoList({ tasks }) {
-
-return (
-    <ScrollView>
+    return (
+      <ScrollView>
         {tasks.map((task, index) => (
-          <Pressable key={index}>
-            <View style={[styles.task, task.completed ? styles.completed : null]}>
-              <Text style={styles.taskText}>{task}</Text>
-            </View>
-          </Pressable>
+            <Pressable key={index}>
+              <View style={[styles.task, task.completed ? styles.completed : null]}>
+                <Text style={styles.taskText}>{task}</Text>
+              </View>
+            </Pressable>
         ))}
-    </ScrollView>
-);
+      </ScrollView>
+    );
 }
+
 const styles = StyleSheet.create({
-    task: {
-      padding: 10,
-      borderBottomWidth: 1,
-      borderColor: '#ccc',
-    },
-    completed: {
-      backgroundColor: '#e0e0e0',
-    },
-    taskText: {
-      fontSize: 16,
-    },
-  });
-  export default ToDoList;
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+});
+
+export default ToDoList;
